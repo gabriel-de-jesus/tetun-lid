257ffe3c82f5cdff4f84bc12e19b66f31a92a5e9
Gabriel de Jesus <gabriel.dejesus@timornews.tl>
2023-03-05 21:57:04 +0000

    Create train-dev-test module

---
+++ training/train_dev_test.py
@@ -0,0 +1,70 @@
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+
+def train_dev_test_split(dataset, test_size_1, test_size_2):
+    """ split data into train, development(dev)/validation and test sets.
+
+    Args:
+        dataset (DataFrame): A DataFrame contained the preprocessed data.
+        test_size_1 (float): the test size proportion between train and (dev+test)
+        test_size_2 (float): the test size proportion between dev and test
+
+    Returns:
+        The proportion of  X(train, dev, test) and the corresponding y sets
+    """
+    corpus = dataset['sentence']
+    labels = dataset['language']
+
+    # Split data into train and dev+test
+    X_train, X_tmp, y_train, y_tmp = train_test_split(
+        corpus, labels, test_size=test_size_1, random_state=42)
+
+    # Split temp data into dev and test sets
+    X_dev, X_test, y_dev, y_test = train_test_split(
+        X_tmp, y_tmp, test_size=test_size_2, random_state=42)
+
+    return X_train, y_train, X_dev, y_dev, X_test, y_test
+
+
+def train_model(convert_features, model_settings, x_train, y_train):
+    """ Transform the text into vector features and fit into model.
+
+    Args:
+        convert_features (function): A function to transform text to vector features.
+        model_settings (function): A ML model with parameters (optional).
+        x_train (series): A X_train proportional data using to train the model.
+        y_train (series): A y_train proportional contains corresponding label of each line of train dataset.
+
+    Returns:
+        A model resulting from the training.
+    """
+    # define pipeline
+    model = Pipeline([
+        ('features_conv', convert_features),
+        ('model_name', model_settings)
+    ])
+
+    # fit the model on the training data
+    model.fit(x_train, y_train)
+
+    return model
+
+
+def evaluate_model(model, x, y):
+    """ Evaluate the model on development/validation set.
+
+    Args:
+        x (series): A X_dev or X_test proportional data using to evaluate the model.
+        y (series): A y_dev or y_text proportional contains corresponding label of each line of dev dataset.
+    """
+    y_pred = model.predict(x)
+
+    print("Accuracy: ", accuracy_score(y, y_pred))
+    print("Confusion Matrix: ", confusion_matrix(y, y_pred))
+    print("Classification Report: ", classification_report(y, y_pred))
